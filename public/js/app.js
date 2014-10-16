4cf56e5eaf446ed0a4e64a24d8bd6d7c2e0b3c5a
Sergey <sv0lapin@gmail.com>
2014-10-17 00:45:36 +0400

    minimal working product

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,16 @@
   var app = angular.module('previewer', []);
 
   app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+    $scope.previews = [];
     $scope.addNew = function() {
-      
+      $http.get('/preview?url=' + encodeURI($scope.newUrl))
+        .success(function(data) {
+          $scope.previews.push(data.path);
+          console.log($scope.previews);
+        })
+        .error(function(data) {
+          $scope.error = data.error;
+        });
     };
   }]);
 })();
