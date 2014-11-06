b8ddfe59b4ef2fde794fb25217d6905bf290de40
Sergey <sv0lapin@gmail.com>
2014-11-06 23:29:48 +0300

    added form disabling while request is pending

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,17 @@
   app.controller('MainController', ['$scope', '$http', function($scope, $http) {
     $scope.previews = [];
     $scope.addNew = function() {
+      $scope.pending = true;
       $http.get('/preview?url=' + encodeURI($scope.newUrl))
         .success(function(data) {
+          $scope.pending = false;
           $scope.previews.push({
             path: data.path,
             url: data.url
           });
         })
         .error(function(data) {
+          $scope.pending = false;
           $scope.error = data.error;
         });
     };
